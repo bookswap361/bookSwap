faa1ee2517780971ef61885beabce9651160e0fd
Lindsey Olmstead <laolmstead19@gmail.com>
2020-07-08 01:05:12 -0400

    Add create and update functionalities to models/swap.js.

--- src/models/swap.js
+++ src/models/swap.js
@@ -17,6 +17,59 @@ Swap.getSwapById = function(id) {
     });
 }
 
+/* To be used to pull swap information for a particular 'traded_by' user 
+in order to check whether the user has multiple "lost in mail" statuses */
+Swap.getSwapByTradedBy = function(user_id) {
+    return new Promise(function(resolve, reject) {
+        mysql.query(getQuery("getSwapByTradedBy"), [user_id])
+            .then(resolve)
+            .catch(reject);
+    });
+}
+
+Swap.createSwap = function(info) {
+    return new Promise(function(resolve, reject) {
+        mysql.query(getQuery("createSwap"), [info.list_id, 
+        info.traded_to, info.traded_by, info.is_accepted,
+        info.request_date, info.approve_date, info.reject_date,
+        info.ship_date, info.received_date, info.status_id])
+            .then(resolve)
+            .catch(reject);
+    });
+}
+
+Swap.updateSwapAccepted = function(info) {
+    return new Promise(function(resolve, reject) {
+        mysql.query(getQuery("updateSwapAccepted"), [info.is_accepted, info.approve_date, info.reject_date, info.swap_id])
+            .then(resolve)
+            .catch(reject);
+    });
+}
+
+Swap.updateSwapShipDate = function(info) {
+    return new Promise(function(resolve, reject) {
+        mysql.query(getQuery("updateSwapShipDate"), [info.ship_date, info.swap_id])
+            .then(resolve)
+            .catch(reject);
+    });
+}
+
+Swap.updateSwapReceivedDate = function(info) {
+    return new Promise(function(resolve, reject) {
+        mysql.query(getQuery("updateSwapReceivedDate"), [info.received_date, info.swap_id])
+            .then(resolve)
+            .catch(reject);
+    });
+}
+
+Swap.updateSwapStatusId = function(info) {
+    return new Promise(function(resolve, reject) {
+        mysql.query(getQuery("updateSwapStatusId"), [info.status_id, info.swap_id])
+            .then(resolve)
+            .catch(reject);
+    });
+}
+
 Swap.deleteSwap = function(id) {
     return new Promise(function(resolve, reject) {
         mysql.query(getQuery("deleteSwap"), [id])
@@ -30,6 +83,27 @@ function getQuery(type) {
     switch(type) {
         case "allSwaps":
             query = "SELECT * FROM swap;"
+            break;
+        case "getSwapByTradedBy":
+            query = "SELECT traded_by, status_id FROM swap WHERE traded_by = ?;"
+            break;
+        case "createSwap":
+            query = "INSERT INTO swap \
+            (list_id, traded_to, traded_by, is_accepted, request_date, \
+            approve_date, reject_date, ship_date, received_date, status_id) \
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
+            break;
+        case "updateSwapAccepted":
+            query = "UPDATE swap SET is_accepted = ?, approve_date = ?, reject_date = ? WHERE swap_id = ?;"
+            break;
+        case "updateSwapShipDate":
+            query = "UPDATE swap SET ship_date = ? WHERE swap_id = ?;"
+            break;
+        case "updateSwapReceivedDate":
+            query = "UPDATE swap SET received_date = ? WHERE swap_id = ?;"
+            break;
+        case "updateSwapStatusId":
+            query = "UPDATE swap SET status_id = ? WHERE swap_id = ?;"
             break;
         case "swapById":
             query = "SELECT * FROM swap WHERE swap_id = ?;"
