f6e1451960208f8939083ff76e3f3cc3acf9d380
addiegoe <58092893+addiegoe@users.noreply.github.com>
2020-07-09 13:58:49 -0700

    Update account.js

--- src/api/account.js
+++ src/api/account.js
@@ -1,18 +1,127 @@
 var express = require("express");
 var router = express.Router();
-var UserServices = require("../services/user");
+var UserServices = require("../services/account");
 
 
 router.route("/")
     .get(function(req, res) {
         id = 1;                                //test variable until sessions are implemented
-        UserServices.getUserById(id)
-            .then(function(user) {
-                res.render('account', user[0]);
+        AccountServices.getAccount(id)
+            .then(function(account) {
+                res.render('account', account[0]);
             })
             .catch(function(err) {
                 res.redirect('/about');
             });
     })
+
+//add books to account
+router.route("/add_books")
+    .post(function(req, res) {
+        AccountServices.addBooks(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/account');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/about');
+            });
+    })
+
+//add books to wishlist
+router.route("/add_wish")
+    .post(function(req, res) {
+        AccountServices.addWish(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/account');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/about');
+            });
+    })
+
+//delete account
+router.route("/delete")
+    .post(function(req, res) {
+        AccountServices.deleteAccount(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/home');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/account');
+            });
+    })
+//delete from books owned
+router.route("/delete_books")
+    .post(function(req, res) {
+        AccountServices.deleteBooks(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/account');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/about');
+            });
+    })
+//delete from wishlist
+router.route("/delete_wish")
+    .post(function(req, res) {
+        AccountServices.deleteWish(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/account');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/about');
+            });
+    })
+//update account info
+router.route("/update")
+    .put(function(req, res) {
+        AccountServices.updateAccount(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/account');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/about');
+            });
+    })
+//update points
+router.route("/update_points")
+    .put(function(req, res) {
+        AccountServices.updatePoints(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/account');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/about');
+            });
+    })
+//update lost limit
+router.route("/update_lost")
+    .put(function(req, res) {
+        AccountServices.updateLostLimit(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/account');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/about');
+            });
+    })
+
+
 
 module.exports = router;
