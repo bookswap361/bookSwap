9dfda221f9a16b36bc5ba042f1e097cf4f738162
Lindsey Olmstead <laolmstead19@gmail.com>
2020-07-09 17:58:41 -0400

    Create a table of available books on /book page

--- src/models/book.js
+++ src/models/book.js
@@ -9,11 +9,30 @@ Book.getAllBooks = function() {
     });
 }
 
+Book.getAvailableBooks = function() {
+    return new Promise(function(resolve, reject) {
+        console.log("Retrieving Books from Database...");
+        mysql.query(getQuery("getAvailableBooks"), [])
+            .then(resolve)
+            .catch(reject);
+    });
+}
+
 function getQuery(type) {
     var query = "";
     switch(type) {
         case "allBooks":
             query = "SELECT * FROM book;"
+            break;
+        case "getAvailableBooks":
+            query = "SELECT \
+                    b.book_id, b.title, a.last_name, a.first_name, COUNT(b.book_id) AS count \
+                    from book b \
+                    INNER JOIN book_author ba ON b.book_id = ba.book_id \
+                    INNER JOIN author a ON ba.author_id = a.author_id \
+                    INNER JOIN books_owned bo ON b.book_id = bo.book_id \
+                    WHERE bo.is_available = 1 \
+                    GROUP BY b.book_id;"
             break;
     }
 
