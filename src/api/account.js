3dbb780572676a053f87844d2f61ccdf1f0ec35d
addiegoe <58092893+addiegoe@users.noreply.github.com>
2020-07-08 19:54:15 -0700

    Update account.js

--- src/api/account.js
+++ src/api/account.js
@@ -15,4 +15,114 @@ router.route("/")
             });
     })
 
+
+//add books to account
+router.route("/add_books")
+    .post(function(req, res) {
+        AccountServices.addBooks(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/account');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/about');
+            });
+    })
+//add to wishlist
+router.route("/add_wish")
+    .post(function(req, res) {
+        AccountServices.addWish(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/account');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/about');
+            });
+    })
+//delete account
+router.route("/delete")
+    .post(function(req, res) {
+        AccountServices.deleteAccount(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/home');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/account');
+            });
+    })
+//delete from books owned
+router.route("/delete_books")
+    .post(function(req, res) {
+        AccountServices.deleteBooks(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/account');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/about');
+            });
+    })
+//delete from wishlist
+router.route("/delete_wish")
+    .post(function(req, res) {
+        AccountServices.deleteWish(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/account');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/about');
+            });
+    })
+//update account info
+router.route("/update")
+    .put(function(req, res) {
+        AccountServices.updateAccount(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/account');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/about');
+            });
+    })
+
+//update points
+router.route("/update_points")
+    .put(function(req, res) {
+        AccountServices.updatePoints(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/account');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/about');
+            });
+    })
+
+//update points
+router.route("/update_lost")
+    .put(function(req, res) {
+        AccountServices.updateLostLimit(req.body)
+            .then(function(result) {
+                if (result) {
+                    res.redirect('/account');
+                }
+            })
+            .catch(function(err) {
+                res.redirect('/about');
+            });
+    })
+
+
+
 module.exports = router;
