ccde072526f08f1b52a15f87f08b5a18a61ec467
Lindsey Olmstead <laolmstead19@gmail.com>
2020-07-09 19:47:45 -0400

    Fix error in sql query

--- src/models/book.js
+++ src/models/book.js
@@ -25,14 +25,14 @@ function getQuery(type) {
             query = "SELECT * FROM book;"
             break;
         case "getAvailableBooks":
-            query = "SELECT \
-                    b.book_id, b.title, a.last_name, a.first_name, COUNT(b.book_id) AS count \
-                    from book b \
-                    INNER JOIN book_author ba ON b.book_id = ba.book_id \
-                    INNER JOIN author a ON ba.author_id = a.author_id \
-                    INNER JOIN books_owned bo ON b.book_id = bo.book_id \
-                    WHERE bo.is_available = 1 \
-                    GROUP BY b.book_id;"
+            query = "select \
+                b.book_id, b.title, a.last_name, a.first_name, count(b.book_id) AS count \
+                from book b \
+                INNER JOIN books_owned bo ON b.book_id = bo.book_id \
+                INNER JOIN book_author ba ON bo.book_id = ba.book_id \
+                INNER JOIN author a ON ba.author_id = a.author_id \
+                WHERE bo.is_available = 1 \
+                GROUP BY b.book_id;"
             break;
     }
 
