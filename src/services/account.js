c84e6d1e0c4996b7f60b489eb7801ee142aced15
addiegoe <58092893+addiegoe@users.noreply.github.com>
2020-07-09 14:02:11 -0700

    promises execute correctly

--- src/services/account.js
+++ src/services/account.js
@@ -1,5 +1,4 @@
 var UserModel = require("../models/user");
-    AccountModel = require("../models/account");
     BooksOwnedModel = require("../models/books_owned");
     WishListModel = require("../models/wishlist");
 var AccountServices = {};
@@ -7,24 +6,39 @@ var AccountServices = {};
 
 
 AccountServices.getAccount = function(id) {
-    return new Promise(function(resolve, reject) {
-        AccountModel.getAccount(id);
-        BooksOwnedModel.getBooks(id);
-        WishListModel.getWishList(id)
-            .then(resolve)
-            .catch(reject);
+    var p1 = new Promise(function(resolve, reject) {
+    UserModel.getUserById(id)
+    .then(resolve)
+    .catch(reject);
     });
+    var p2 = new Promise(function(resolve, reject) {
+    BooksOwnedModel.getBooks(id)
+    .then(resolve)
+    .catch(reject);
+    });
+    var p3 = new Promise(function(resolve, reject) {
+    WishListModel.getWishList(id)
+    .then(resolve)
+    .catch(reject);
+    });
+return Promise.all([p1, p2, p3])
 };
 
 //add one point when user adds a book -- not sure if this works
 AccountServices.addBook = function(body, id) {
-    return new Promise(function(resolve, reject) {
-        BooksOwnedModel.addBook(body);
-        UserModel.updatePoints(1, id)
-            .then(resolve)
-            .catch(reject);
+    var p1 = new Promise(function(resolve, reject) {
+    BooksOwnedModel.addBook(body)
+    .then(resolve)
+    .catch(reject);
     });
+    var p2 = new Promise(function(resolve, reject) {
+    UserModel.updatePoints(id, 1)
+    .then(resolve)
+    .catch(reject);
+    });
+return Promise.all([p1, p2])
 };
+
 
 AccountServices.addWish = function(body) {
     return new Promise(function(resolve, reject) {
@@ -36,14 +50,24 @@ AccountServices.addWish = function(body) {
 
 
 AccountServices.deleteAccount = function(body) {
-    return new Promise(function(resolve, reject) {
-        BooksOwnedModel.deleteAllBooks(body);
-        WishListModel.deleteAllWish(body);
-        UserModel.deleteUser(body)
-            .then(resolve)
-            .catch(reject);
+    var p1 = new Promise(function(resolve, reject) {
+    BooksOwnedModel.deleteAllBooks(body)
+    .then(resolve)
+    .catch(reject);
     });
+    var p2 = new Promise(function(resolve, reject) {
+    WishListModel.deleteAllWish(body)
+    .then(resolve)
+    .catch(reject);
+    });
+    var p3 = new Promise(function(resolve, reject) {
+    UserModel.deleteUser(body)
+    .then(resolve)
+    .catch(reject);
+    });
+return Promise.all([p1, p2, p3])
 };
+
 
 AccountServices.deleteBooks = function(body) {
     return new Promise(function(resolve, reject) {
