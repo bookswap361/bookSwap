5f0ad0c5c08d7ea1e5d209777fe559cb4e84fcd5
addiegoe <58092893+addiegoe@users.noreply.github.com>
2020-07-08 19:56:10 -0700

    adds sessions

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -3,11 +3,13 @@ var app = express();
 var handlebars = require("express-handlebars").create({"defaultLayout": "main"});
 var bodyParser = require("body-parser");
 var path = require("path");
+var session = require("express-session");
 
 var settings = function(app) {
     app.engine("handlebars", handlebars.engine);
     app.use(bodyParser.urlencoded({"extended": true}));
     app.use(bodyParser.json());
+    app.use(session({secret:"noOneKnows"}));
     app.use(express.static(path.join(process.cwd(), "/public")));
     app.set("views", path.join(process.cwd(), "/public/views"));
     app.set("view engine", "handlebars");
