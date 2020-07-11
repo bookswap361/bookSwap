8c777a5fa661b05a415fa3496d3308eb39f5e493
Lindsey Olmstead <laolmstead19@gmail.com>
2020-07-11 03:45:18 -0400

    Add draft swap table to Swaps tab on /account.

--- public/scripts/account.js
+++ public/scripts/account.js
@@ -1,6 +1,8 @@
 
 /*Reference: https://www.w3schools.com/howto/howto_js_tabs.asp*/
 
+
+// Books tab functionality.
 document.getElementById("booksTab").addEventListener("click", function(event){
 	
 	var tabcontent = document.getElementsByClassName("tabcontent");
@@ -17,6 +19,7 @@ document.getElementById("booksTab").addEventListener("click", function(event){
 	event.currentTarget.className += " active";
 });
 
+// Swap tab functionality.
 document.getElementById("swapsTab").addEventListener("click", function(event){
 	
 	var tabcontent = document.getElementsByClassName("tabcontent");
@@ -31,8 +34,28 @@ document.getElementById("swapsTab").addEventListener("click", function(event){
 
 	document.getElementById("swaps").style.display = "block";
 	event.currentTarget.className += " active";
+
+	// Make get request to return table of swaps
+	/*var request = new XMLHttpRequest();
+	return new Promise(function(resolve, reject){
+		var id = 1;		// to be updated
+		var url = "/" + id;
+	})*/
+
+	var id = 1;
+	var url = "../swap/" + id;
+	fetch(url, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}).then(function (response) {
+		console.log(response);
+		return response;
+	})
 });
 
+// Wish List functionality.
 document.getElementById("wishlistTab").addEventListener("click", function(event){
 	
 	var tabcontent = document.getElementsByClassName("tabcontent");
