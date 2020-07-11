c6ac8417d775315c3b0011707b47eeb87b7a97e3
Jane Fong <janeefongg@gmail.com>
2020-07-10 20:29:39 -0700

    update date format

--- src/models/forum.js
+++ src/models/forum.js
@@ -16,7 +16,7 @@ function getQuery(type) {
             query = "SELECT t.thread_id, t.title, u.first_name, u.last_name, DATE_FORMAT(t.create_date,'%M-%D-%Y') as create_date, t.is_resolved FROM thread t INNER JOIN user u ON t.user_id = u.user_id GROUP BY t.thread_id ORDER BY t.thread_id;";
             break;
         case "threadById":
-            query = "SELECT m.message_id, m.thread_id, u.first_name, u.last_name, m.post, m.date FROM messages m INNER JOIN user u ON m.user_id = u.user_id WHERE m.thread_id = ? ORDER BY m.date ASC;"
+            query = "SELECT m.message_id, m.thread_id, u.first_name, u.last_name, m.post, DATE_FORMAT(m.date, '%M-%D-%Y') as date FROM messages m INNER JOIN user u ON m.user_id = u.user_id WHERE m.thread_id = ? ORDER BY m.date ASC;"
     }
     return query;
 };
