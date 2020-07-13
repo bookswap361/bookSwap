1c4f3e804230cba7b51f45fddd0bc7c8e7c94291
Jane Fong <janeefongg@gmail.com>
2020-07-13 13:11:07 -0700

    uses hbs helpers to display resolve status

--- public/scripts/forum.js
+++ public/scripts/forum.js
@@ -3,7 +3,6 @@ ClassicEditor
         "removePlugins": ["ImageUpload", "MediaEmbed"]
     })
     .then(function(editor) {
-        editor.resize("75%");
         editor.model.document.on("change:data", function() {
             var input = document.getElementById("post-input");
             input.value = editor.getData();
