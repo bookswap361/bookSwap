00524a85e17a86f5ebb6dd497006ee3affc5402d
Jane Fong <janeefongg@gmail.com>
2020-07-13 08:57:52 -0700

    renames forum helper files

---
+++ src/helpers/forum.js
@@ -0,0 +1,12 @@
+var ForumHbsHelpers = {};
+
+ForumHbsHelpers.displayResolveButton = function(userId, ownerId, domId) {
+    // if userId == ownerId
+    // create button and append
+};
+
+ForumHbsHelpers.formatThreadLink = function(threadId) {
+    return "/forum/?id=" + threadId;
+};
+
+module.exports = ForumHbsHelpers;
