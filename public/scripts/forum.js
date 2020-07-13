7dbb081ef91961d0f9fe5219e2fc81c333137d46
Jane Fong <janeefongg@gmail.com>
2020-07-13 11:23:11 -0700

    adds basic styling for thread page

--- public/scripts/forum.js
+++ public/scripts/forum.js
@@ -3,7 +3,7 @@ ClassicEditor
         "removePlugins": ["ImageUpload", "MediaEmbed"]
     })
     .then(function(editor) {
-        console.log(editor)
+        editor.resize("75%");
         editor.model.document.on("change:data", function() {
             var input = document.getElementById("post-input");
             input.value = editor.getData();
