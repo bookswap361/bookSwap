0d0492f7fb83ab02a9f584519eec565df51dd4c4
addiegoe <58092893+addiegoe@users.noreply.github.com>
2020-07-14 09:15:25 -0700

    Update account.js

--- src/api/account.js
+++ src/api/account.js
@@ -1,14 +1,15 @@
 var express = require("express");
 var router = express.Router();
 var AccountServices = require("../services/account");
-
+var UserServices = require("../services/user");
+var session = require('express-session');
 
 router.route("/")
     .get(function(req, res) {
-        id = 1;                                //test variable until sessions are implemented
+var id =  session.u_id;
         AccountServices.getAccount(id)
-            .then(function(account) {
-                let merged = {...account[0], ...account[1], ...account[2], ...account[3]};
+            .then(function(account) { 
+let merged = {...account[0], ...account[1], ...account[2], ...account[3]};
                 res.render('account', merged);
             })
             .catch(function(err) {
