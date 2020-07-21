41404d398bfabbde865f548d730eae78b6ab4112
Lindsey Olmstead <laolmstead19@gmail.com>
2020-07-21 17:35:56 -0400

    Prevent user from making swaps with insufficient points

--- src/api/books_available.js
+++ src/api/books_available.js
@@ -15,18 +15,25 @@ router.get("/", function(req, res) {
 router.post("/condition", function(req, res) {
     BooksAvailableServices.getCondition(req.body, req.session.u_id)
         .then(function(books) {
-            console.log("api/books condition: ");
-            console.log(books);
             res.send(books);
         }).catch(function(err) {
             res.status(400).json({"error": err});
         });
 });
 
+router.get("/get-points", function(req, res) {
+     BooksAvailableServices.getPoints(req.session.u_id)
+         .then(function(result) {
+             res.send(result);
+         })
+         .catch(function(err) {
+             res.status(400).json({"error": err});
+         });
+ });
+
 router.post("/add-swap", function(req, res) {
     BooksAvailableServices.addSwap(req.body, req.session.u_id)
         .then(function(result) {
-            console.log("Adding swap api/books_owned...");
             res.redirect('/');
         }).catch(function(err) {
             res.status(400).json({"error": err});
--- src/services/books_available.js
+++ src/services/books_available.js
@@ -5,7 +5,6 @@ BooksAvailableServices.getAvailableBooks = function(user_id) {
 	return new Promise(function(resolve, reject) {
 		BooksAvailableModel.getAvailableBooks(user_id)
 			.then(function(results) {
-				console.log("Processing Books in BookServices...");
 				var availableBooks = []
 				results.forEach(function(book) {
 					availableBooks.push({
@@ -26,7 +25,6 @@ BooksAvailableServices.getCondition = function(book_id, user_id) {
     return new Promise(function(resolve, reject){
         BooksAvailableModel.getCondition(book_id, user_id)
             .then(function(results){
-                console.log("Processing in services/books...");
                 var books = [];
                 results.forEach(function(item) {
                     books.push({
@@ -37,7 +35,6 @@ BooksAvailableServices.getCondition = function(book_id, user_id) {
                       "cost": item.cost
                   });
                 })
-                console.log(books);
                 return books;
             })
             .then(resolve)
@@ -45,9 +42,20 @@ BooksAvailableServices.getCondition = function(book_id, user_id) {
     });
 }
 
+BooksAvailableServices.getPoints = function(user_id) {
+     return new Promise(function(resolve, reject){
+         BooksAvailableModel.getPoints(user_id)
+             .then(function(results){
+                 var points = {"points": results[0].points};
+                 return points;
+             })
+             .then(resolve)
+             .catch(reject);
+     });
+ }
+
 BooksAvailableServices.addSwap = function(info, user_id) {
 	return new Promise(function(resolve, reject){
-    console.log("Adding swap in services/books_available..");
 		BooksAvailableModel.addSwap(info, user_id)
 			.then(resolve)
 			.catch(reject);
