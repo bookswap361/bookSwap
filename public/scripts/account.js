1dcd9723ee6854393eb6cc83199dcdc1da2c60ac
addiegoe <58092893+addiegoe@users.noreply.github.com>
2020-07-30 14:12:32 -0700

    adds carousel animation

--- public/scripts/account.js
+++ public/scripts/account.js
@@ -282,5 +282,32 @@ document.getElementById("wishlistTab").addEventListener("click", showWishList);
 document.getElementById('addInventory').addEventListener("click", function(event) {
     location.href = "/book/search"});
 
+/*Carousel slideshow*/
+/*Source: https://www.w3schools.com/howto/howto_js_slideshow.asp*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += "active";
+}
 
 window.onload = document.getElementById("booksTab").click();
