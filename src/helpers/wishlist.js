45cf5dd59ad60ec87cbdbfd39f0e73261123b697
Jane Fong <janeefongg@gmail.com>
2020-07-31 11:56:49 -0700

    adds wishlist helper

---
+++ src/helpers/wishlist.js
@@ -0,0 +1,17 @@
+var WishlistHbsHelpers = {};
+
+WishlistHbsHelpers.displayButton = function(wishlist, bookId) {
+    var display = true;
+    for (var i = 0; i < wishlist.length; i++) {
+        console.log(wishlist[i])
+        var list = wishlist[i];
+        if (list.book_id == bookId) {
+            display = false;
+            break;
+        }
+    }
+
+    return display ? "<button type='button' class='btn btn-primary' id='addWishBtn' onclick='addBook.call(null, \"wishlist\")'>Add to my wishlist</button>" : "";
+};
+
+module.exports = WishlistHbsHelpers;
