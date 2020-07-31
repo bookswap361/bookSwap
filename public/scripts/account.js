a52f2d3c10119ca44b4a0a6e8c30a1c5781a690e
addiegoe <58092893+addiegoe@users.noreply.github.com>
2020-07-31 11:07:53 -0700

    fixes reference error

--- public/scripts/account.js
+++ public/scripts/account.js
@@ -206,7 +206,6 @@ function showSlides(n) {
   }
   
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += "active";
 }
 
 document.getElementById("profileTab").addEventListener("click", showProfile);
