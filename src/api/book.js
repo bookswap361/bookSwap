9131dafe7c978da4a546b53e7a283f6dc2a587ad
Jane Fong <janeefongg@gmail.com>
2020-07-30 17:36:46 -0700

    updates error handling for book route

--- src/api/book.js
+++ src/api/book.js
@@ -4,29 +4,26 @@ var BookServices = require("../services/book"),
     UserServices = require("../services/user");
 
 router.route("/create-book")
-    .get(function(req, res) {
+    .get(function(req, res, next) {
         BookServices.getOlKeys()
             .then(function(result){
                 var keys = [];
                 result.forEach(function(element){
                     keys.push(element.ol_key);
                 });
-                console.log("Keys found:");
-                console.log(keys);
                 res.render("createbook", {"keys": keys});
             })
             .catch(function(err){
-                res.status(400).json({"error": err});
+                next(err);
             });
     })
-    .post(function(req, res) {
+    .post(function(req, res, next) {
         BookServices.createBook(req.body)
             .then(function(result) {
-                console.log('post new book??')
                 res.redirect("/book/" + req.body.bol_key + "?new=1");
             })
             .catch(function(err) {
-                res.status(400).json({"error": err});
+                next(err);
             });
     });
 
@@ -36,55 +33,45 @@ router.route("/search")
     });
 
 router.route("/:id")
-    .get(function(req, res) {
-        console.log("Searching for: ", req.params, req.query)
+    .get(function(req, res, next) {
         BookServices.getBookByOLId(req.params.id, req.session.u_id)
             .then(function(result) {
-                if(result[0].length > 0){
-                    console.log("Result found");
+                if (result[0].length > 0) {
                     result[0][0].exists = 1;
-                    console.log(result);
                     res.render("book-page", req.query && req.query.new ? {"result": result[0][0], "isNew": true} : {"result": result[0][0], "copies": result[1]});
                 } else {
-                    res.send("404 Error - Try refreshing or go home")
+                   next(err);
                 }
             })
             .catch(function(err) {
-                console.error(err);
-                res.status(400).json({"error": err});
+                next(err);
             });
     })
-    .post(function(req, res) {
+    .post(function(req, res, next) {
         var data = req.body;
         BookServices.getBookByOLId(req.body.book_id, req.session.u_id)
             .then(function(result) {
                 if(result[0].length > 0){
-                    console.log("Result found");
                     result[0][0].exists = 1;
                     res.render("book-page", {"result": result[0][0], "copies": result[1], "wishlist": result[2]});
                 } else {
                     data.new = 1;
-                    console.log("No book found");
-                    console.log(data);
                     res.render("book-page", {"result": data});
                 }
             })
             .catch(function(err) {
-                console.error(err);
-                res.status(400).json({"error": err});
+                next(err);
             });
     });
 
 router.route("/create-author")
-    .post(function(req, res) {
-        console.log(req.body);
+    .post(function(req, res, next) {
         BookServices.createAuthor(req.body)
-            .then(function(result) {
-                console.log("Author created");
-                res.send("Author created");
+            .then(function() {
+                res.status(200).send("Author created");
             })
             .catch(function(err) {
-                res.status(400).json({"error": err});
+                next(err);
             });
     });
 
