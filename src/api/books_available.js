d6dc76597d797169c3eb3e5bae38d354a3361a91
Jane Fong <janeefongg@gmail.com>
2020-08-01 13:32:12 -0700

    updates books_available route for filter

--- src/api/books_available.js
+++ src/api/books_available.js
@@ -3,13 +3,25 @@ var router = express.Router();
 var BooksAvailableServices = require("../services/books_available");
 
 router.get("/", function(req, res, next) {
-    BooksAvailableServices.getAvailableBooks(req.session.u_id)
-        .then(function(result) {
-            res.render("book", {"books": result});
-        })
-        .catch(function(err) {
-            next(err);
-        });
+    if (req.query && req.query.filter && req.query.filter == "afford") {
+        BooksAvailableServices.getAffordableBooks(req.session.u_id)
+            .then(function(result) {
+                console.log("at the end", result)
+                res.render("book", {"books": result, "criteria": req.query.filter});
+            })
+            .catch(function(err) {
+                next(err);
+            });
+    } else {
+        BooksAvailableServices.getAvailableBooks(req.session.u_id)
+            .then(function(result) {
+                console.log(result)
+                res.render("book", {"books": result, "criteria": "all"});
+            })
+            .catch(function(err) {
+                next(err);
+            });
+    }
 });
 
 router.get("/condition/:id", function(req, res, next) {
