9b72c634d8ba5257b1935ed42954b7d41d82870f
Jane Fong <janeefongg@gmail.com>
2020-08-01 13:33:53 -0700

    add query to get books available to user by points

--- src/models/book.js
+++ src/models/book.js
@@ -70,6 +70,10 @@ Book.getAvailableBooks = function(user_id) {
     return mysql.query(getQuery("getAvailableBooks"), [user_id]);
 }
 
+Book.getAvailableBooksByPoints = function(userId, points) {
+    return mysql.query(getQuery("getAvailableBooksByPoints"), [userId, points]);
+};
+
 function getQuery(type) {
     var query = "";
     switch(type) {
@@ -114,6 +118,15 @@ function getQuery(type) {
                 WHERE bo.is_available = 1 AND bo.user_id != ? \
                 GROUP BY b.book_id;";
             break;
+        case "getAvailableBooksByPoints":
+            query = "SELECT b.book_id, b.title, a.name, bc.cost, count(b.book_id) AS count \
+                from book b \
+                INNER JOIN books_owned bo ON b.book_id = bo.book_id \
+                INNER JOIN book_condition bc ON bo.condition_id = bc.condition_id \
+                INNER JOIN book_author ba ON bo.book_id = ba.book_id \
+                INNER JOIN author a ON ba.author_id = a.author_id \
+                WHERE bo.is_available = 1 AND bo.user_id != ? AND bc.cost <= ?\
+                GROUP BY b.book_id;";
         }
 
     return query;
