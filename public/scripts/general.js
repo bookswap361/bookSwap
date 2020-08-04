fb474af73df70c20966d098839dcb52cba138bde
Jane Fong <janeefongg@gmail.com>
2020-08-04 10:43:22 -0700

    adds condition for content-type if openlibrary

--- public/scripts/general.js
+++ public/scripts/general.js
@@ -2,11 +2,13 @@ function fetchHelper(url, method, data = {}) {
 	return fetch(url, {
 		method: method,
 		headers: {
-			"Content-Type": "application/json"
+            ...(!(url.match(/\b(openlibrary)\b/g)) && {"Content-Type": "application/json"})
 		},
-		//body: JSON.stringify(data)
         ...(method !== "GET" && {body: JSON.stringify(data)})
 	})
+    .catch(function(err) {
+        console.log(err)
+    })
 };
 
 function windowReload() {
