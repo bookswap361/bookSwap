b0f87a0e60618005d94c0d71460546e17306ef8d
Jane Fong <janeefongg@gmail.com>
2020-08-04 08:24:20 -0700

    creates general script file

---
+++ public/scripts/general.js
@@ -0,0 +1,13 @@
+function fetchHelper(url, method, data = {}) {
+	return fetch(url, {
+		method: method,
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(data)
+	})
+};
+
+function windowReload() {
+    window.location.reload();
+};
