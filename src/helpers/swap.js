7e608a5cacdf02835cd43112f138af324c74cdfa
Jane Fong <janeefongg@gmail.com>
2020-08-04 11:49:32 -0700

    breaks up swap status function to use smaller helpers

--- src/helpers/swap.js
+++ src/helpers/swap.js
@@ -1,55 +1,81 @@
 var SwapHbsHelpers = {};
 
-SwapHbsHelpers.getStatus = function(userId, swapId, tradedToId, tradedById, approveDate, rejectDate, shipDate, receiveDate, notReceived, lostLimit, refundDate, isComplete, hasClaim, lostDate) {
-    var returnHtml = "";
-    if (userId == tradedById) { // For the shipper
-        if (isComplete) {
-            returnHtml += (rejectDate ? "Rejected" : lostDate ? "Lost in Transit" : refundDate ? "Refunded" : "Complete");
-        } else if (!(approveDate || rejectDate)) {
-            returnHtml += "<button type='submit' form='accept-trade' name='swapId' value=" + swapId + " class='btn btn-outline-primary'>Accept Trade</button>";
-            returnHtml += "<button type='submit' form='reject-trade' name='swapId' value=" + swapId + " class='btn btn-outline-danger'>Reject Trade</button>";
-        } else if (approveDate && !shipDate) {
-            returnHtml += "<button type='submit' form='get-shipping' name='swapId' value=" + swapId + " class='btn btn-outline-danger'>Get Shipping Address</button>";
-            returnHtml += "<button type='submit' form='shipped' name='swapId' value=" + swapId + " class='btn btn-outline-primary'>Mark Shipped</button>";
-        } else if (shipDate && !receiveDate && !notReceived) {
-            returnHtml += "In Transit";
-        } else if (shipDate && notReceived && !lostLimit) {
-            returnHtml += "<button type='submit' form='lost' name='swapId' value=" + swapId + " class='btn btn-outline-primary'>Lost in Transit</button>";
-        } else if (hasClaim) {
-            returnHtml += "Open Claim";
-        }
+SwapHbsHelpers.getStatus = function(swapObject) {
+    if (isShipper(swapObject)) { // For the shipper
+        return generateShipperStatus(swapObject);
     } else { // For the receiver
-        if (isComplete) {
-            returnHtml += (rejectDate ? "Rejected" : lostDate ? "Lost in Transit" : refundDate ? "Refunded" : "Complete");
-        } else if (!(approveDate || rejectDate)) {
-            returnHtml += "Approval Pending";
-        } else if (rejectDate) {
-            returnHtml += "Rejected";
-        } else if (approveDate && !shipDate) {
-            returnHtml += "Shipment Pending";
-        } else if (shipDate && !receiveDate && !notReceived) {
-            var today = new Date();
-            var ship = new Date(shipDate);
-            var milliseconds = today - ship;
-            var dayDiff = parseInt(milliseconds / (1000 * 60 * 60 * 24));
-            var minuteDiff = parseInt(milliseconds / (1000 * 60)); // For test
-            if (minuteDiff > 1) { // 'Show Not Received' button after 4 minutes for test, change to use dayDiff later
-                if (lostLimit) {
-                    returnHtml += "<button type='submit' form='not-received-refund' name='swapId' value=" + swapId + " class='btn btn-outline-primary'>Not Received</button>";
-                } else {
-                    returnHtml += "<button type='submit' form='not-received' name='swapId' value=" + swapId + " class='btn btn-outline-primary'>Not Received</button>";
-                }
-            } else {
-                returnHtml += "<button type='submit' form='received' name='swapId' value=" + swapId + " class='btn btn-outline-primary'>Received</button>";
-                returnHtml += "<button type='submit' form='claim' name='swapId' value=" + swapId + " class='btn btn-outline-danger'>Received in Poor Condition</button>";
-           }
-        } else if (shipDate && notReceived) {
-            returnHtml += "Mark Not Received -- Pending Shipper";
-        } else if (hasClaim) {
-            returnHtml += "Open Claim";
-        }
+        return generateReceiverStatus(swapObject);
     }
+};
 
+function isShipper(swapObject) {
+    return swapObject.user_id == swapObject.traded_by;
+};
+
+function getCompleteStatus(swapObject) {
+    return swapObject.reject_date ? "Rejected" : swapObject.lost_date ? "Lost in Transit" : swapObject.refund_date ? "Refunded" : "Complete";
+};
+
+function getButtonHtml(swapId, form, isPrimary, text) {
+    return "<button type='submit' form=" + form + " name='swapId' value=" + swapId + " class='btn btn-outline-" + (isPrimary ? "primary" : "danger") + "'>" + text + "</button>";
+};
+
+function generateShipperStatus(swapObject) {
+    var returnHtml = "";
+    if (swapObject.is_complete) {
+        returnHtml += getCompleteStatus(swapObject);
+    } else if (!(swapObject.approve_date || swapObject.reject_date)) {
+        returnHtml += getButtonHtml(swapObject.swap_id, "accept-trade", true, "Accept Trade");
+        returnHtml += getButtonHtml(swapObject.swap_id, "reject-trade", false, "Reject Trade");
+    } else if (swapObject.approve_date && !swapObject.ship_date) {
+        returnHtml += getButtonHtml(swapObject.swap_id, "get-shipping", true, "Get Shipping Address");
+        returnHtml += getButtonHtml(swapObject.swap_id, "shipped", true, "Mark Shipped");
+    } else if (swapObject.ship_date && !swapObject.received_date && !swapObject.not_received) {
+        returnHtml += "In Transit";
+    } else if (swapObject.ship_date && swapObject.not_received && !swapObject.lost_limit) { 
+        returnHtml += getButtonHtml(swapObject.swap_id, "lost", true, "Lost In Transit");
+    } else if (swapObject.has_claim) {
+        returnHtml += "Open Claim";
+    }
+    return returnHtml;
+};
+
+function getTimeElapsed(shipDate) {
+    var today = new Date();
+    var ship = new Date(shipDate);
+    var milliseconds = today - ship;
+    var dayDiff = parseInt(milliseconds / (1000 * 60 * 60 * 24));
+    var minuteDiff = parseInt(milliseconds / (1000 * 60)); // For test
+    return minuteDiff; // Return dayDiff when done
+};
+
+function generateReceiverStatus(swapObject) {
+    var returnHtml = "";
+    if (swapObject.is_complete) {
+        returnHtml += getCompleteStatus(swapObject);
+    } else if (!(swapObject.approve_date || swapObject.reject_date)) {
+        returnHtml += "Approval Pending";
+    } else if (swapObject.reject_date) {
+        returnHtml += "Rejected";
+    } else if (swapObject.approve_date && !swapObject.ship_date) {
+        returnHtml += "Shipment Pending";
+    } else if (swapObject.ship_date && !swapObject.received_date && !swapObject.not_received) {
+        var elapsedTime = getTimeElapsed(swapObject.ship_date)
+        if (elapsedTime > 1) { // 'Show Not Received' button after 4 minutes for test, change to use dayDiff later
+            if (swapObject.lost_limit) {
+                returnHtml += getButtonHtml(swapObject.swap_id, "not-received-refund", true, "Not Received");
+            } else {
+                returnHtml += getButtonHtml(swapObject.swap_id, "not-received", true, "Not Received");
+            }
+        } else {
+            returnHtml += getButtonHtml(swapObject.swap_id, "received", true, "Received");
+            returnHtml += getButtonHtml(swapObject.swap_id, "claim", false, "Received in Poor Condition");
+        }
+    } else if (swapObject.ship_date && swapObject.not_received) {
+        returnHtml += "Mark Not Received -- Pending Shipper";
+    } else if (swapObject.has_claim) {
+        returnHtml += "Open Claim";
+    }
     return returnHtml;
 };
 
