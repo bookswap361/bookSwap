295c1811f0e6b6bd081c555bfa85a2efb01f171e
addiegoe <58092893+addiegoe@users.noreply.github.com>
2020-08-10 11:39:51 -0700

    Update user.js

--- src/api/user.js
+++ src/api/user.js
@@ -2,17 +2,29 @@ var express = require("express");
 var router = express.Router();
 var UserServices = require("../services/user");
 
-router.route("/user")
+router.route("/")
     .get(function(req, res) {
-        res.render("/user");
+        res.render("user");
     })
 
 //TODO
-router.route("/search")
+router.route("/search_by_name")
     .get(function(req, res, next) {
-        UserServices.searchUsers(req.session.u_id, req.params.query, req.params.search_by)
+        UserServices.searchUsersByName(req.query.name)
             .then(function(result) {
-                res.render('/search', result);
+                console.log(result);
+                res.render('search_user', result);
+            })
+            .catch(function(err) {
+                next(err);
+            })
+    })
+
+router.route("/search_by_email")
+    .get(function(req, res, next) {
+        UserServices.searchUsersByEmail(req.query.email)
+            .then(function(result) {
+                res.render('search_user', result);
             })
             .catch(function(err) {
                 next(err);
