b40a79b6443212f6e17aa22b5b9d918e1c91f62f
Lindsey Olmstead <laolmstead19@gmail.com>
2020-08-10 02:00:38 -0400

    Add genre dropdown menu to books page

--- src/api/books_available.js
+++ src/api/books_available.js
@@ -1,12 +1,16 @@
 var express = require("express");
 var router = express.Router();
-var BooksAvailableServices = require("../services/books_available");
+var BooksAvailableServices = require("../services/books_available"),
+    BookServices = require("../services/book");
 
 router.get("/", function(req, res, next) {
     if (req.query && req.query.filter && req.query.filter == "afford") {
         BooksAvailableServices.getAffordableBooks(req.session.u_id)
             .then(function(result) {
-                res.render("book", {"books": result, "criteria": req.query.filter});
+                BookServices.getGenreList()
+                    .then(function(genres) {
+                        res.render("book", {"books": result, "criteria": req.query.filter, "genres": genres});
+                    })
             })
             .catch(function(err) {
                 next(err);
@@ -14,7 +18,10 @@ router.get("/", function(req, res, next) {
     } else {
         BooksAvailableServices.getAvailableBooks(req.session.u_id)
             .then(function(result) {
-                res.render("book", {"books": result, "criteria": "all"});
+                BookServices.getGenreList()
+                    .then(function(genres) {
+                        res.render("book", {"books": result, "criteria": "all", "genres": genres});
+                    })
             })
             .catch(function(err) {
                 next(err);
